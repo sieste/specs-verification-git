2b4e24baca6eb5eb0ef4532d630b365c07ed2a9b
Stefan Siegert <stefan_siegert@gmx.de>
2014-01-02 14:10:21 +0000

    added affine kernel dressing as dressing.method

--- DressEnsemble.R
+++ DressEnsemble.R
@@ -1,10 +1,13 @@
-DressEnsemble <- function(ens, dressing.method="silverman", parameters=NA) {
+DressEnsemble <- function(ens, dressing.method="silverman", 
+                          parameters=NA) 
+{
 
+  # silverman's rule of thumb
   if (dressing.method == "silverman") {
-    # silverman's rule of thumb
-    n.members <- rowSums(1 - is.na(ens))
+    n.members <- rowSums(!is.na(ens))
     if (any(n.members==1)) {
-      warning("Some ensembles have only one member. The kernel width is set to zero for these.")
+      warning(paste("Some ensembles have only one member.",
+                    "The kernel width is set to zero for these."))
     }
     stdevs <- apply(ens, 1, sd, na.rm=TRUE)
     ker.wd <- (4 * stdevs^5 / (3 * n.members))^0.2
@@ -12,6 +15,25 @@ DressEnsemble <- function(ens, dressing.method="silverman", parameters=NA) {
     K <- max(n.members)
     ker.wd <- matrix(rep(ker.wd, K), ncol=K)
 
+    ker.type <- "gauss"
+  }
+
+  # affine kernel dressing
+  #
+  #       p(y|x) = 1 / K * sum {dnorm(y, z.i(x), s(x))}
+  # where   s(x) = s1 + s2 * (4 / 3 / K) ^ 0.4 * var(x)
+  # and   z.i(x) = r0 + r1 * mean(x) + r2 * x[i]
+  if (dressing.method=="akd") {
+    stopifnot(all(names(parameters) %in% c("r0", "r1", "r2", "s1", "s2")))
+    k <- rowSums(!is.na(ens))
+    var.ens <- apply(ens, 1, var, na.rm=TRUE)
+    z <- with(parameters, r0 + r1 * rowMeans(ens, na.rm=TRUE) + r2 * ens)
+    s2 <- with(parameters, s1 + s2 * (4 / 3 / k) ^ .4 * var.ens)
+    s <- sapply(s2, function(x) sqrt(max(x, 0)))
+ 
+    K <- max(k)
+    ker.wd <- matrix(rep(s, K), ncol=K)
+    ens <- z
     ker.type <- "gauss"
   }
 
