f987395d974c06e7df5408658580bb965598d9e0
Stefan Siegert <stefan_siegert@gmx.de>
2014-01-20 15:49:42 +0000

    renamed ver to obs

--- R/FairBrier.R
+++ R/FairBrier.R
@@ -3,22 +3,22 @@
 # THE FAIR BRIER SCORE
 #
 # ens ... ensemble matrix (N*K)
-# ver ... verification vector (N)
+# obs ... observation vector (N)
 # tau ... exceedance threshold that defines the event
 #
 # reference: Ferro (2013) "Fair scores for ensemble forecasts"
 #
 ################################
-FairBrier <- function(ens, ver, tau=0.5) {
-  ver <- matrix(ver, ncol=1)
+FairBrier <- function(ens, obs, tau=0.5) {
+  obs <- matrix(obs, ncol=1)
   if (is.null(dim(ens))) {
     ens <- matrix(ens, nrow=1)
   }
-  stopifnot(nrow(ens) == nrow(ver))
+  stopifnot(nrow(ens) == nrow(obs))
 
   K <- ncol(ens)
   i <- rowSums(ens > tau)
-  j <- 1 * (ver > tau)
+  j <- 1 * (obs > tau)
   fb <- (j - i / K) ^ 2 - i * (K - i) / K / K / (K - 1)
   return(fb)
 }
