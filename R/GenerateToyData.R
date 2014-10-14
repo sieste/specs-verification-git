b5fb91a77ba1d2906b4a4388070ac85aa65a87c3
sieste <stefan_siegert@gmx.de>
2014-10-14 22:50:13 +0200

    renamed GenerateToyData arguments

--- R/GenerateToyData.R
+++ R/GenerateToyData.R
@@ -1,9 +1,9 @@
-GenerateToyData <- function(N = 20, mu.y = 0, s.s = 7, s.eps = 6, mu.x1 = 0, beta1 = 0.2, s.eta1 = 8, K1 = 10, mu.x2 = NA, beta2 = NA, s.eta2 = NA, K2 = NA) {
+GenerateToyData <- function(N = 20, mu.y = 0, s.s = 7, s.eps = 6, mu.x = 0, beta = 0.2, s.eta = 8, K = 10, mu.x.ref = NA, beta.ref = NA, s.eta.ref = NA, K.ref = NA) {
   s <- rnorm(N, 0, s.s)
   y <- mu.y + s + rnorm(N, 0, s.eps)
-  x1 <- mu.x1 + beta1 * s + matrix(rnorm(N*K1, 0, s.eta1), N, K1)
-  if (!any(is.na(c(mu.x2, beta2, s.eta2, K2)))) {
-    x2 <- mu.x2 + beta2 * s + matrix(rnorm(N*K2, 0, s.eta2), N, K2)
+  x1 <- mu.x + beta * s + matrix(rnorm(N*K, 0, s.eta), N, K)
+  if (!any(is.na(c(mu.x.ref, beta.ref, s.eta.ref, K.ref)))) {
+    x2 <- mu.x.ref + beta.ref * s + matrix(rnorm(N*K.ref, 0, s.eta.ref), N, K.ref)
     ret <- list(obs=y, ens=x1, ens.ref=x2)
   } else {
     ret <- list(obs=y, ens=x1)
