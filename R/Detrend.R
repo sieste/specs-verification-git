10c17403918ce1f3b033eeab96259264b2e575f7
sieste <stefan_siegert@gmx.de>
2015-06-04 22:38:45 +0100

    cleaner implementation of detrending

--- R/Detrend.R
+++ R/Detrend.R
@@ -1,13 +1,22 @@
 Detrend <- function(x, demean=TRUE) {
+  # detrend using the row means
   if (class(x) == "matrix" | class(x) == "data.frame") {
-    xx <- rowMeans(x)
+    xx <- rowMeans(x, na.rm=TRUE)
   } else {
     xx <- x
   }
   N <- length(xx)
-  trnd <- fitted(lm(xx~(t=1:N)))
-  # if demean is false, add mean back to x-trend
-  m <- ifelse(demean, 0, mean(unlist(x)))
+  if (N == 1) {
+    # if only one time step is given, detrending amounts to substracting the mean
+    trnd <- x
+  } else {
+    # otherwise estimate a linear function of time and take the fitted values
+    # as "the trend" 
+    lmod <- lm(xx~(t=1:N))
+    trnd <- drop(cbind(1, xx) %*% coef(lmod))
+  }
+  # if demean is false, add the grand mean back to x minus trend
+  m <- ifelse(demean, 0, mean(unlist(x), na.rm=TRUE))
   return(x - trnd + m)
 }
 
